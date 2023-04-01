b4da6ff7d03f3a3349f9baf937f9791c90507694
Usuario <Usuario@DESKTOP-MPT5JJT>
2023-04-01 18:11:48 +0200

    Class Dentist variables

--- src/POJO/Dentists.java
+++ src/POJO/Dentists.java
@@ -1,4 +1,19 @@
 package POJO;
 
-public class Dentists {
+import java.io.Serializable;
+
+public class Dentists implements Serializable{
+
+
+	private static final long serialVersionUID = -4597260876619998054L;
+	
+	private Integer doc_id;
+	private String name;
+	private String bank_account;
+	private Integer doc_mobile;
+	private String doc_email;
+	private List<Appointment> appointments;
+	
+	
+	
 }
